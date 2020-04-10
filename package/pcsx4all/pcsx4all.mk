d0af7493c3b76b98619179c19f7b0a33bc9f1d2a
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-10 03:28:43 +0100

    pcsx4all: Switch to tonyjih/RG350_pcsx4all
    gameblabla's version has some known issues, such as save states failing
    sometimes

--- package/pcsx4all/pcsx4all.mk
+++ package/pcsx4all/pcsx4all.mk
@@ -3,8 +3,8 @@
 # pcsx4all
 #
 ################################################################################
-PCSX4ALL_VERSION = 21084ac
-PCSX4ALL_SITE = $(call github,gameblabla,pcsx4all,$(PCSX4ALL_VERSION))
+PCSX4ALL_VERSION = a2ae76c6f1
+PCSX4ALL_SITE = $(call github,tonyjih,RG350_pcsx4all,$(PCSX4ALL_VERSION))
 PCSX4ALL_AUTORECONF = YES
 PCSX4ALL_INSTALL_STAGING = YES
 
@@ -24,7 +24,7 @@ PCSX4ALL_MAKE_OPTS = \
   GCC_NEW=1
 
 define PCSX4ALL_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(PCSX4ALL_MAKE_OPTS) $(MAKE) -C $(@D) -f Makefile.$(PCSX4ALL_PLATFORM)
+	$(TARGET_MAKE_ENV) $(MAKE) $(PCSX4ALL_MAKE_OPTS) -C $(@D) -f Makefile.$(PCSX4ALL_PLATFORM)
 endef
 
 define PCSX4ALL_INSTALL_OPK_IMAGES
