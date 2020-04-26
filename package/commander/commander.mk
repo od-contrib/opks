370a8213ec90c2d4b83c8a403883216663a1da5e
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-26 12:59:00 +0100

    commander: update + system fallback font

--- package/commander/commander.mk
+++ package/commander/commander.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COMMANDER_VERSION = 7ea0831
+COMMANDER_VERSION = c9c03ba
 COMMANDER_SITE = $(call github,od-contrib,commander,$(COMMANDER_VERSION))
 COMMANDER_DEPENDENCIES = sdl sdl_gfx sdl_image sdl_ttf
 
@@ -12,9 +12,10 @@ COMMANDER_TARGET_PLATFORM ?= $(call qstrip,$(BR2_PACKAGE_COMMANDER_TARGET_PLATFO
 
 ifeq ($(BR2_TARGET_PLATFORM),"rg350")
 COMMANDER_TARGET_PLATFORM=rg350
+COMMANDER_OPK_EXT=gcw0
 else ifeq ($(BR2_TARGET_PLATFORM),"gcw0")
 COMMANDER_TARGET_PLATFORM=gcw0
-else ifeq ($(BR2_TARGET_PLATFORM),"retrofw")
+else ifeq ($(BR2_TARGET_PLATFORM),"rs90")
 COMMANDER_TARGET_PLATFORM=rs90
 else ifeq ($(BR2_TARGET_PLATFORM),"retrofw")
 COMMANDER_TARGET_PLATFORM=retrofw
@@ -22,10 +23,18 @@ else
 $(error No platform mapping for $(BR2_TARGET_PLATFORM))
 endif
 
+ifeq ($(COMMANDER_OPK_EXT),)
+COMMANDER_OPK_EXT=$(COMMANDER_TARGET_PLATFORM)
+endif
+
 COMMANDER_CONF_OPTS += \
 	-DTARGET_PLATFORM=$(COMMANDER_TARGET_PLATFORM) \
-	-DWITH_SYSTEM_SDL_TTF=ON \
-	-DWITH_SYSTEM_SDL_GFX=ON
+	-DWITH_SYSTEM_SDL_GFX=ON \
+	-DFONTS=$(BR2_PACKAGE_COMMANDER_FONTS)
+
+ifneq ($(BR2_TARGET_DEVICE),"rg350m")
+COMMANDER_CONF_OPTS += -DWITH_SYSTEM_SDL_TTF=ON
+endif
 
 ifneq ($(BR2_PACKAGE_COMMANDER_AUTOSCALE),"")
 COMMANDER_CONF_OPTS += -DAUTOSCALE=$(BR2_PACKAGE_COMMANDER_AUTOSCALE)
@@ -55,8 +64,16 @@ endif
 
 define COMMANDER_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/opks
-	cd $(@D) && ./package-opk.sh $(COMMANDER_TARGET_PLATFORM) $(COMMANDER_BUILDDIR) \
-	  $(BINARIES_DIR)/opks/$(COMMANDER_OPK_NAME).opk
+	cd $(@D) && mksquashfs \
+	  "opkg/default.$(COMMANDER_OPK_EXT).desktop" \
+	  "opkg/readme.$(COMMANDER_OPK_EXT).txt" \
+	  opkg/commander.png \
+	  res/*.png \
+	  res/Fiery_Turk.ttf \
+	  res/FreeSans.ttf \
+	  "$(COMMANDER_BUILDDIR)/commander" \
+	  "$(BINARIES_DIR)/opks/$(COMMANDER_OPK_NAME).opk" \
+	  -all-root -no-xattrs -noappend -no-exports
 endef
 
 $(eval $(cmake-package))
